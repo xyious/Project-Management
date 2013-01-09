fdc8cad54036e4e15e2cae61fafc86d6d1738e39
Alexander Knopf <xyious@gmail.com>
2013-01-09 12:33:07 +0100

    neue dateien

--- CreateWorkUnitTable.php
+++ CreateWorkUnitTable.php
@@ -1,6 +1,6 @@
 <?php
 include('include/PDOConnect.php');
-$success = $connection->query("CREATE TABLE IF NOT EXISTS workunits (ID INT AUTO_INCREMENT PRIMARY KEY, description TEXT, progress INT, estimated_hours INT, deadline DATETIME)");
+$success = $connection->query("CREATE TABLE IF NOT EXISTS workunits (ID INT AUTO_INCREMENT PRIMARY KEY, project_id INT, description TEXT, progress INT, estimated_hours INT, deadline DATETIME, status INT, type INT)");
 
 if ($success) {
 	echo 'Table "workunits" created';
--- include/CreateWorkUnitForm.php
+++ include/CreateWorkUnitForm.php
@@ -1,12 +1,12 @@
 <form id='CreateProject' action='/Project/CreateWorkUnit.php' method='post' accept-charset='UTF-8'>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
 <table rules="all" border="1px" style="width: 100%; border-color: #FFFFFF">
-  <tr><td><label for='Customer'>Kunde:</label></t:>
-      <td><textarea Cols="100" rows="5" name='Customer' id='Customer'></textarea></td></tr>
-  <tr><td><label for='ProjectDescription'>Projektbeschreibung:</label></t:>
-      <td><textarea Cols="100" rows="25" name='ProjectDescription' id='ProjectDescription'></textarea></td></tr>
-  <tr><td><label for='ProjectDeadline'>Termin:</label></t:>
-      <td><input type='date' name='ProjectDeadline' id='ProjectDeadline'  maxlength="50" /></td></tr>
+  <tr><td><label for='WUDescription'>Arbeitspaketbeschreibung:</label></td>
+      <td><textarea Cols="100" rows="25" name='WUDescription' id='WUDescription'></textarea></td></tr>
+  <tr><td><label for='WUDeadline'>Termin:</label></td>
+      <td><input type='date' name='WUDeadline' id='WUDeadline'  maxlength="50" /></td></tr>
+  <tr><td><label for='Estimate'>Voraussichtlicher Aufwand (Stunden):</label></td>
+      <td><input type='text' name='Estimate' id='Estimate'  maxlength="50" /></td></tr>
     <tr><td>&nbsp;</td><td><input type='submit' name='Submit' value='Submit' /></td>
   </tr>
 </form>
