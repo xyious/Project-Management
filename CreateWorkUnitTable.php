1267980cf04b468bedcffe66ac8da76bef84ea8b
xyious <xyious@gmail.com>
2013-01-09 13:22:02 +0100

    Update CreateWorkUnitTable.php

--- CreateWorkUnitTable.php
+++ CreateWorkUnitTable.php
@@ -1,6 +1,6 @@
 <?php
 include('include/PDOConnect.php');
-$success = $connection->query("CREATE TABLE IF NOT EXISTS workunits (ID INT AUTO_INCREMENT PRIMARY KEY, project_id INT, description TEXT, progress INT, estimated_hours INT, deadline DATETIME, status INT, type INT)");
+$success = $connection->query("CREATE TABLE IF NOT EXISTS workunits (ID INT AUTO_INCREMENT PRIMARY KEY, project_id INT, creation TIMESTAMP default CURRENT_TIMESTAMP, description TEXT, progress INT, estimated_hours INT, deadline DATETIME, status INT, type INT)");
 
 if ($success) {
 	echo 'Table "workunits" created';
