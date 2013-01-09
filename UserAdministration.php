01971fd2a4ea245922ff303d3d80469857a83a13
Alexander Knopf <xyious@gmail.com>
2013-01-09 13:45:49 +0100

    new file:   UserAdministration.php

---
+++ UserAdministration.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if (!$_SESSION['IP']) {
+	$_SESSION['IP'] = $_SERVER['REMOTE_ADDR']; }
+?>
+<!doctype html>
+<html>
+<head>
+<title>Projekt Details</title>
+<link rel="stylesheet" href="include/body.css" type="text/css">
+</head>
+<body>
+<h1>Arbeitspakete:</h1>
+<table rules="all" border="1px" style="width: 100%; border-color: #FFFFFF">
+  <tr>
+    <th>Erstellungsdatum</th>
+    <th>Beschreibung</th>
+    <th>Verantwortlicher</th>
+    <th>Termin</th>
+  </tr>
+<?php
+include "include/PDOConnect.php";
+$workunit_query = $connection->prepare("SELECT * FROM workunits");
+$assignment_query = $connection->prepare("SELECT * FROM workunits INNER JOIN worker_assignment on workunits.ID = worker_assignment.workunit_ID WHERE workunits.ID = :workunit_id AND worker_assignment.job = 3");
+$workunit_query->execute();
+for ($i = 0; $i < $workunit_query->rowCount(); $i++)
+{
+	$row = $workunit_query->fetch();
+	echo "<tr><td>" . $row['creation'] . "</td>";
+	echo "<td><a href='WUDetails.php?id=" . $row['ID'] . "'>" . $row['description'] . "</a></td>";
+	echo "<td>FIX ME</td>";
+	echo "<td>" . $row['deadline'] . "</td></tr>";
+}
+?>
+</table>
+<br>
+<?php include "include/CreateWorkUnitForm.php" ?>
+</body>
+</html>
