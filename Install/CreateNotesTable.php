100c2a147db9380b5e98cc60b1f7df5a402f1664
xyious <xyious@gmail.com>
2013-01-11 17:41:35 +0100

    Update Install/CreateNotesTable.php

--- Install/CreateNotesTable.php
+++ Install/CreateNotesTable.php
@@ -1,6 +1,5 @@
-
 <?php
-include('include/PDOConnect.php');
+include('../include/PDOConnect.php');
 $table = 'notes';
 $success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, workunit_ID INT, note TEXT)");
 
