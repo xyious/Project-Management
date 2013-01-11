ef221f4e802f20fba008d4303ebf8e5ec9a4057d
xyious <xyious@gmail.com>
2013-01-11 17:43:15 +0100

    Update Install/CreateWorkerAssignmentTable.php

--- Install/CreateWorkerAssignmentTable.php
+++ Install/CreateWorkerAssignmentTable.php
@@ -1,5 +1,5 @@
 <?php
-include('include/PDOConnect.php');
+include('../include/PDOConnect.php');
 $table = 'worker_assignment';
 $success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, user_ID INT, workunit_ID INT, project_ID INT, job INT)");
 if ($success) {
