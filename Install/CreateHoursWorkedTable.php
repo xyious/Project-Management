cf6ed3ad950f9a7ea06aacfc6b1e8550ca5d0367
xyious <xyious@gmail.com>
2013-01-11 17:41:08 +0100

    Update Install/CreateHoursWorkedTable.php

--- Install/CreateHoursWorkedTable.php
+++ Install/CreateHoursWorkedTable.php
@@ -1,5 +1,5 @@
 <?php
-include('include/PDOConnect.php');
+include('../include/PDOConnect.php');
 $table = 'hours_worked';
 $success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, user_id INT, workunit_id INT, duration FLOAT, billable_duration FLOAT, date DATETIME, description TEXT)");
 
