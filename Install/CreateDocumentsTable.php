a5b0049773c9f36ae7f11b14e0a163311895ffad
xyious <xyious@gmail.com>
2013-01-11 17:40:54 +0100

    Update Install/CreateDocumentsTable.php

--- Install/CreateDocumentsTable.php
+++ Install/CreateDocumentsTable.php
@@ -1,6 +1,5 @@
-
 <?php
-include('include/PDOConnect.php');
+include('../include/PDOConnect.php');
 $table = 'documents';
 $success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, workunit_ID INT, link VARCHAR(256), description TEXT)");
 
