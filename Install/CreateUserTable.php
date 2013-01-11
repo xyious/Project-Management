0b22bfc663958d15e471255c16593aecf8ca481f
xyious <xyious@gmail.com>
2013-01-11 17:42:02 +0100

    Update Install/CreateUserTable.php

--- Install/CreateUserTable.php
+++ Install/CreateUserTable.php
@@ -1,6 +1,5 @@
-
 <?php
-include('include/PDOConnect.php');
+include('../include/PDOConnect.php');
 $table = 'users';
 $success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, username varchar(25), password varchar(128), email varchar(256), displayname varchar(64))");
 
