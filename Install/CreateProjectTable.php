29322c48047bdf7e7001d9e3a85a6c0ab8ffbb2a
xyious <xyious@gmail.com>
2013-01-11 17:41:48 +0100

    Update Install/CreateProjectTable.php

--- Install/CreateProjectTable.php
+++ Install/CreateProjectTable.php
@@ -1,6 +1,5 @@
-
 <?php
-include('include/PDOConnect.php');
+include('../include/PDOConnect.php');
 $table = 'project';
 $success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, status INT, deadline TIMESTAMP, customer VARCHAR(256), description TEXT)");
 
