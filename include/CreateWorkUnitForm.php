de76b2065069a30e561ca184146d891e5647424a
Alexander Knopf <xyious@gmail.com>
2013-01-16 11:17:31 +0100

    Update include/CreateWorkUnitForm.php

--- include/CreateWorkUnitForm.php
+++ include/CreateWorkUnitForm.php
@@ -5,18 +5,7 @@
   <tr><td><label for='WUDescription'>Arbeitspaketbeschreibung:</label></td>
       <td><textarea Cols="100" rows="25" name='WUDescription' id='WUDescription'></textarea></td></tr>
   <tr><td><label for='Responsible'>Arbeitspaket Verantwortlicher:</label></td>
-      <td><select name="Responsible" multiple>
-<?php 
-echo "test";
-include "PDOConnect.php";
-$query=$connection->prepare("Select ID, displayname FROM users");
-for ($i = 0; $i < $query->rowCount(); $i++)
-{
-  echo "test1";
-  $row = $query->fetch();
-  echo "<option value=\"" . $row['ID'] . "\">" . $row['displayname'] . "</option>";
-}
-?>
+      <td><select name="Responsible" multiple>$$SelectList
       </select></td></tr>
   <tr><td><label for='WUDeadline'>Termin:</label></td>
       <td><input type='date' name='WUDeadline' id='WUDeadline'  maxlength="50" /></td></tr>
