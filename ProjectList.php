dc420e409ce81e384757eed28a7dbf7d2b8d69df
Alexander Knopf <xyious@gmail.com>
2013-01-22 17:43:12 +0100

    Integrated CreateProjectForm

--- ProjectList.php
+++ ProjectList.php
@@ -10,6 +10,7 @@ if (!$_SESSION['IP']) {
 <link rel="stylesheet" href="include/body.css" type="text/css">
 </head>
 <body>
+<h2>Projektliste:</h2>
 <table rules="all" border="1px" style="width: 100%; border-color: #FFFFFF">
   <tr>
     <th>Erstellungsdatum</th>
@@ -30,6 +31,33 @@ for ($i = 0; $i < $query->rowCount(); $i++)
 	echo "<td>" . $row['deadline'] . "</td></tr>";
 }
 ?>
+</table><br>
+<h2>Neues Project erstellen:</h2><br>
+<form id='CreateProject' action='/Project/CreateProject.php' method='post' accept-charset='UTF-8'>
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+<table rules="all" border="1px" style="width: 100%; border-color: #FFFFFF">
+  <tr><td><label for='Customer'>Kunde:</label></t:>
+      <td><textarea Cols="100" rows="5" name='Customer' id='Customer'></textarea></td></tr>
+  <tr><td><label for='ProjectDescription'>Projektbeschreibung:</label></t:>
+      <td><textarea Cols="100" rows="25" name='ProjectDescription' id='ProjectDescription'></textarea></td></tr>
+        <tr><td><label for='Responsible'>Projektverantwortlicher:</label></td>
+  <td><select multiple name="Responsible" size="1">
+<?php
+$user_query = $connection->prepare("Select ID, displayname FROM users");
+$user_query->execute();
+for ($i = 0; $i < $user_query->rowCount(); $i++)
+{
+	$row = $user_query->fetch();
+	echo "<option value=\"" . $row['ID'] . "\">" . $row['displayname'] . "</option>";
+}
+?>
+      </select></td></tr>
+  <tr><td><label for='ProjectDeadline'>Termin:</label></t:>
+      <td><input type='date' name='ProjectDeadline' id='ProjectDeadline'  maxlength="50" /></td></tr>
+    <tr><td>&nbsp;</td><td><input type='submit' name='Submit' value='Submit' /></td>
+  </tr>
+</form>
 </table>
+
 </body>
 </html>
