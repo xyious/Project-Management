f95dd4924980c33911eb5b94151bf560d517084e
Alexander Knopf <xyious@gmail.com>
2013-01-23 20:17:37 +0100

    Update include/LoginForm.php

--- include/LoginForm.php
+++ include/LoginForm.php
@@ -1,5 +1,6 @@
 <form id='login' action='/Project/Login.php' method='post' accept-charset='UTF-8'>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
+<input type='hidden' name='previous' id='previous' value=<?php echo "'" . $_GET['previous']. "'";/>
 <table rules="all" border="1px" style="width: 100%; border-color: #FFFFFF">
   <tr><td><label for='username' >Benutzername:</label></t:>
       <td><input type='text' name='username' id='username'  maxlength="50" /></td></tr>
