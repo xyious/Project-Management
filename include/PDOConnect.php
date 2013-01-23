c237e02a9ef5beb9fffdced562e63e9c91f07703
Alexander Knopf <xyious@gmail.com>
2013-01-23 12:33:34 +0100

    Update include/PDOConnect.php

--- include/PDOConnect.php
+++ include/PDOConnect.php
@@ -2,4 +2,5 @@
 	$dsn = 'mysql:host=localhost;dbname=ProjectManagement';
 	$connection = new PDO($dsn, 'Projectadmin', 'ProjAdmin!');
 	$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$connection->setAttribute(PDO::ATTR_PERSISTENT, true);
 ?>
