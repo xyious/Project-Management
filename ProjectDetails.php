f464170482cb94fb0a9485313869800356b565da
Alexander Knopf <xyious@gmail.com>
2013-01-23 13:23:55 +0100

    Update ProjectDetails.php

--- ProjectDetails.php
+++ ProjectDetails.php
@@ -22,13 +22,12 @@ if (!$_SESSION['IP']) {
 include "include/PDOConnect.php";
 $user_query = $connection->prepare("Select ID, displayname FROM users");
 $user_query->execute();
+$users;
 for ($i = 0; $i < $user_query->rowCount(); $i++)
 {
 	$row = $user_query->fetch();
-	$users[$i][0] = $row['ID'];
-	$users[$i][1] = $row['displayname'];
+	$users[$row['ID'] => $row['displayname'];
 }
-while (userlist($id, $displayname) = mysql_fetch_row($result));
 $workunit_query = $connection->prepare("SELECT * FROM workunits WHERE project_ID = :project_id");
 $workunit_query->bindParam(':project_id', $_GET['id'], PDO::PARAM_STR);
 $assignment_query = $connection->prepare("SELECT users.displayname FROM workunits INNER JOIN worker_assignment on workunits.ID = worker_assignment.workunit_ID INNER JOIN users on worker_assignment.user_ID = users.ID WHERE workunits.ID = :workunit_id AND worker_assignment.job = 3");
